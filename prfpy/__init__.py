8b84ea060ef96a46a39a00f17a68f686f3eee7d1
Tomas Knapen <tknapen@gmail.com>
2019-03-31 19:48:26 +0200

    added rudimentary docs

--- prfpy/__init__.py
+++ prfpy/__init__.py
@@ -1,11 +1,11 @@
-from gauss import gauss1D_cart, \
+from .gauss import gauss1D_cart, \
                 gauss1D_log, \
                 gauss2D_iso_cart, \
                 gauss2D_rot_cart
-from timecourse import convolve_stimulus_dm, \
+from .timecourse import convolve_stimulus_dm, \
                         stimulus_through_prf, \
                         generate_arima_noise, \
                         sgfilter_predictions, \
                         generate_random_legendre_drifts, \
                         generate_random_cosine_drifts
-from stimulus import PRFStimulus2D, PRFStimulus1D
+from .stimulus import PRFStimulus2D, PRFStimulus1D
