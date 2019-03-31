15d2d5450478ed87f3d71899b91888b206c96875
Tomas Knapen <tknapen@gmail.com>
2019-03-31 19:24:11 +0200

    updated setup, version for develop install, and added To Do and Policy checklist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
 # Get version and release info, which is all stored in nideconv/version.py
-ver_file = os.path.join('nideconv', 'version.py')
+ver_file = os.path.join('prfpy', 'version.py')
 with open(ver_file) as f:
     exec(f.read())
 
