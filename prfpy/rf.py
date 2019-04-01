dd01a60394cbb5fda190320b2341f8382962eaf4
Tomas Knapen <tknapen@gmail.com>
2019-04-01 19:02:45 +0200

    after running autopep8

--- prfpy/rf.py
+++ prfpy/rf.py
@@ -1,9 +1,10 @@
 import numpy as np
 import scipy.stats as stats
 
+
 def gauss1D_cart(x, mu=0.0, sigma=1.0):
     """gauss1D_cart
-    
+
     gauss1D_cart takes a 1D array x, a mean and standard deviation,
     and produces a gaussian with given parameters, with a peak of height 1.
 
@@ -15,7 +16,7 @@ def gauss1D_cart(x, mu=0.0, sigma=1.0):
         mean/mode of gaussian (the default is 0.0)
     sigma : float, optional
         standard deviation of gaussian (the default is 1.0)
-    
+
     Returns
     -------
     numpy.ndarray
@@ -24,9 +25,10 @@ def gauss1D_cart(x, mu=0.0, sigma=1.0):
 
     return np.exp(-((x-mu)**2)/(2*sigma**2))
 
+
 def gauss1D_log(x, mu=0.0, sigma=1.0):
     """gauss1D_log
-    
+
     gauss1D_log takes a 1D array x, a mean and standard deviation,
     and produces a pRF with given parameters with the distance between mean and x log-scaled 
 
@@ -38,7 +40,7 @@ def gauss1D_log(x, mu=0.0, sigma=1.0):
         mean/mode of gaussian (the default is 0.0)
     sigma : float, optional
         standard deviation of gaussian (the default is 1.0)
-    
+
     Returns
     -------
     numpy.ndarray
@@ -47,9 +49,10 @@ def gauss1D_log(x, mu=0.0, sigma=1.0):
 
     return np.exp(-(np.log(x/mu)**2)/(2*sigma**2))
 
+
 def vonMises1D(x, mu=0.0, kappa=1.0):
     """vonMises1D
-    
+
     vonMises1D takes a 1D array x, a mean and kappa (inverse of standard deviation),
     and produces a von Mises pRF with given parameters. This shape can be thought of 
     as a circular gaussian shape. Used for orientation or motion direction pRFs, 
@@ -65,23 +68,24 @@ def vonMises1D(x, mu=0.0, kappa=1.0):
     kappa : float, optional
         dispersion coefficient of the von Mises, 
         akin to invers of standard deviation of gaussian (the default is 1.0)
-    
+
     Returns
     -------
     numpy.ndarray
         von Mises values at x, peak has y-value of 1
     """
-    vm = stats.vonmises.pdf(x-mu,kappa)
-    
+    vm = stats.vonmises.pdf(x-mu, kappa)
+
     return vm / np.max(vm)
 
-def gauss2D_iso_cart(x, y, mu=(0.0,0.0), sigma=1.0):
+
+def gauss2D_iso_cart(x, y, mu=(0.0, 0.0), sigma=1.0):
     """gauss2D_iso_cart
-    
+
     gauss2D_iso_cart takes two-dimensional arrays x and y, containing
     the x and y coordinates at which to evaluate the 2D isotropic gaussian 
     function, with a given sigma, and returns a 2D array of Z values.
-        
+
     Parameters
     ----------
     x : numpy.ndarray, 2D
@@ -92,23 +96,24 @@ def gauss2D_iso_cart(x, y, mu=(0.0,0.0), sigma=1.0):
         mean, 2D coordinates of mean/mode of gauss (the default is (0.0,0.0))
     sigma : float, optional
         standard deviation of gauss (the default is 1.0
-    
-    Returns
+
+    Returns 
     -------
     numpy.ndarray, 2D
         gaussian values evaluated at (x,y)
     """
-    
+
     return np.exp(-((x-mu[0])**2 + (x-mu[1])**2)/(2*sigma**2))
 
-def gauss2D_rot_cart(x, y, mu=(0.0,0.0), sigma=1.0, theta=0.0, ar=1.0):
+
+def gauss2D_rot_cart(x, y, mu=(0.0, 0.0), sigma=1.0, theta=0.0, ar=1.0):
     """gauss2D_rot_cart
-    
+
     gauss2D_rot_cart takes two-dimensional arrays x and y, containing
     the x and y coordinates at which to evaluate the 2D non-isotropic gaussian 
     function, with a given sigma, angle of rotation theta, and aspect ratio ar.
     it returns a 2D array of Z values. Default is an isotropic gauss.
-        
+
     Parameters
     ----------
     x : numpy.ndarray, 2D
@@ -123,7 +128,7 @@ def gauss2D_rot_cart(x, y, mu=(0.0,0.0), sigma=1.0, theta=0.0, ar=1.0):
         angle of rotation of gauss (the default is 0.0)   
     ar : float, optional
         aspect ratio of gauss, multiplies the rotated y parameters (the default is 1.0)
-    
+
     Returns
     -------
     numpy.ndarray, 2D
@@ -134,16 +139,17 @@ def gauss2D_rot_cart(x, y, mu=(0.0,0.0), sigma=1.0, theta=0.0, ar=1.0):
 
     return np.exp(-(xr**2 + ar**2 * yr**2)/(2*sigma**2))
 
-def gauss2D_logpolar(ecc, polar, mu=(1.0,0.0), sigma=1.0, kappa=1.0):
+
+def gauss2D_logpolar(ecc, polar, mu=(1.0, 0.0), sigma=1.0, kappa=1.0):
     """gauss2D_logpolar
-    
+
     gauss2D_logpolar takes two-dimensional arrays ecc and polar, containing
     the eccentricity and polar angle coordinates at which to evaluate the 2D gaussian, 
     which in this case is a von Mises in the polar angle direction, and a log gauss 
     in the eccentricity dimension, and returns a 2D array of Z values.
     We recommend entering the ecc and polar angles ordered as x and y for easy
     visualization.
-        
+
     Parameters
     ----------
     ecc : numpy.ndarray, 2D
@@ -157,16 +163,15 @@ def gauss2D_logpolar(ecc, polar, mu=(1.0,0.0), sigma=1.0, kappa=1.0):
     kappa : float, optional
         dispersion coefficient of the von Mises, 
         akin to invers of standard deviation of gaussian (the default is 1.0)
-        
+
     Returns
     -------
     numpy.ndarray, 2D
         values evaluated at (ecc, polar), peak has y-value of 1.
     """
     ecc_gauss = np.exp(-(np.log(ecc/mu[0])**2)/(2*sigma**2))
-    polar_von_mises = stats.vonmises.pdf(polar-mu[1],kappa)
+    polar_von_mises = stats.vonmises.pdf(polar-mu[1], kappa)
     polar_von_mises /= np.max(polar_von_mises)
     logpolar_Z = ecc_gauss * polar_von_mises
 
     return logpolar_Z / np.max(logpolar_Z)
-
--- prfpy/timecourse.py
+++ prfpy/timecourse.py
@@ -2,53 +2,56 @@ import numpy as np
 import scipy.signal as signal
 from statsmodels.tsa.arima_process import arma_generate_sample
 
+
 def convolve_stimulus_dm(stimulus, hrf):
     """convolve_stimulus_dm
-    
+
     convolve_stimulus_dm convolves an N-D (N>=2) stimulus array with an hrf
-    
+
     Parameters
     ----------
     stimulus : numpy.ndarray, N-D (N>=2) 
         stimulus experimental design, with the final dimension being time
     hrf : numpy.ndarray, 1D
         contains kernel for convolution
-    
+
     """
     hrf_shape = np.ones(len(stimulus.shape))
     hrf_shape[-1] = hrf.shape[0]
 
-    return signal.fftconvolve(stimulus, hrf.reshape(hrf_shape), mode='full', axes=(-1))[...,:stimulus.shape[-1]]
+    return signal.fftconvolve(stimulus, hrf.reshape(hrf_shape), mode='full', axes=(-1))[..., :stimulus.shape[-1]]
+
 
 def stimulus_through_prf(prfs, stimulus):
     """stimulus_through_prf
-    
+
     dot the stimulus and the prfs
-        
+
     Parameters
     ----------
     prfs : numpy.ndarray
         the array of prfs. 
     stimulus : numpy.ndarray
         the stimulus design matrix, either convolved with hrf or not.
-    
+
     """
     assert prfs.shape[1:] == stimulus.shape[:-1], \
         'prf array dimensions {prfdim} and input stimulus array dimensions {stimdim} must have same dimensions'.format(
-            prfdim=prfs.shape[1:], 
+            prfdim=prfs.shape[1:],
             stimdim=stimulus.shape[:-1])
     prf_r = prfs.reshape((prfs.shape[0], -1))
     stim_r = stimulus.reshape((-1, stimulus.shape[-1]))
     return prf_r @ stim_r
 
-def generate_arima_noise(ar=(1,0.4), 
-                        ma=(1,0.4), 
-                        dimensions=(1000,120),
-                        **kwargs):
+
+def generate_arima_noise(ar=(1, 0.4),
+                         ma=(1, 0.4),
+                         dimensions=(1000, 120),
+                         **kwargs):
     """generate_arima_noise
-    
+
     generate_arima_noise creates temporally correlated noise
-    
+
     Parameters
     ----------
     ar : tuple, optional
@@ -61,10 +64,10 @@ def generate_arima_noise(ar=(1,0.4),
         the first dimension is the nr of separate timecourses, the second dimension
         is the timeseries length.
         (the default is (1000,120), a reasonable if brief length for an fMRI run)
-    
+
     **kwargs are passed on to statsmodels.tsa.arima_process.arma_generate_sample
 
-    Returns
+    Returns 
     -------
     numpy.ndarray
         noise of requested dimensions and properties
@@ -72,11 +75,12 @@ def generate_arima_noise(ar=(1,0.4),
     """
     return np.array([arma_generate_sample(ar, ma, dimensions[1], **kwargs) for i in range(dimensions[0])])
 
+
 def sgfilter_predictions(predictions, window_length=201, polyorder=3, highpass=True, **kwargs):
     """sgfilter_predictions
-    
+
     savitzky golay filter predictions, to conform to data filtering
-    
+
     Parameters
     ----------
     predictions : numpy.ndarray
@@ -89,28 +93,29 @@ def sgfilter_predictions(predictions, window_length=201, polyorder=3, highpass=T
         when the window length is longer than 2 minutes)
 
     **kwargs are passed on to scipy.signal.savgol_filter
-    
+
     Raises
     ------
     ValueError
         when window_length is even
-    
+
     Returns
     -------
     numpy.ndarray
         filtered version of the array
     """
-    if window_length%2 != 1:
-        raise ValueError # window_length should be odd
-    lp_filtered_predictions = signal.savgol_filter(predictions, window_length=window_length, polyorder=polyorder, **kwargs)
+    if window_length % 2 != 1:
+        raise ValueError  # window_length should be odd
+    lp_filtered_predictions = signal.savgol_filter(
+        predictions, window_length=window_length, polyorder=polyorder, **kwargs)
     if highpass:
         return predictions - lp_filtered_predictions
     else:
         return lp_filtered_predictions
 
 
-def generate_random_legendre_drifts(dimensions=(1000,120), 
-                                    amplitude_ranges=[[500,600],[-50,50],[-20,20],[-10,10],[-5,5]]):
+def generate_random_legendre_drifts(dimensions=(1000, 120),
+                                    amplitude_ranges=[[500, 600], [-50, 50], [-20, 20], [-10, 10], [-5, 5]]):
     """generate_random_legendre_drifts
 
     generate_random_legendre_drifts generates random slow drifts
@@ -132,15 +137,17 @@ def generate_random_legendre_drifts(dimensions=(1000,120),
         random multiplication factors that created the drifts
     """
     nr_polys = len(amplitude_ranges)
-    drifts = np.polynomial.legendre.legval(x=np.arange(dimensions[-1]), c=np.eye(nr_polys)).T
+    drifts = np.polynomial.legendre.legval(
+        x=np.arange(dimensions[-1]), c=np.eye(nr_polys)).T
     drifts = (drifts-drifts.mean(0))/drifts.mean(0)
-    drifts[:,0] = np.ones(drifts[:,0].shape)
+    drifts[:, 0] = np.ones(drifts[:, 0].shape)
     random_factors = np.array([ar[0] + (ar[1]-ar[0])/2.0 + (np.random.rand(dimensions[0])-0.5) * (ar[1]-ar[0])
-                            for ar in amplitude_ranges])
+                               for ar in amplitude_ranges])
     return np.dot(drifts, random_factors), random_factors
 
-def generate_random_cosine_drifts(dimensions=(1000,120), 
-                                    amplitude_ranges=[[500,600],[-50,50],[-20,20],[-10,10],[-5,5]]):
+
+def generate_random_cosine_drifts(dimensions=(1000, 120),
+                                  amplitude_ranges=[[500, 600], [-50, 50], [-20, 20], [-10, 10], [-5, 5]]):
     """generate_random_cosine_drifts
 
     generate_random_cosine_drifts generates random slow drifts
@@ -162,8 +169,8 @@ def generate_random_cosine_drifts(dimensions=(1000,120),
         random multiplication factors that created the drifts
     """
     nr_freqs = len(amplitude_ranges)
-    x = np.linspace(0,np.pi,dimensions[-1])
+    x = np.linspace(0, np.pi, dimensions[-1])
     drifts = np.array([np.cos(x*f) for f in range(nr_freqs)]).T
     random_factors = np.array([ar[0] + (ar[1]-ar[0])/2.0 + (np.random.rand(dimensions[0])-0.5) * (ar[1]-ar[0])
-                            for ar in amplitude_ranges])
-    return np.dot(drifts, random_factors), random_factors    
+                               for ar in amplitude_ranges])
+    return np.dot(drifts, random_factors), random_factors
