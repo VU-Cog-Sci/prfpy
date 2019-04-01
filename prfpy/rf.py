d7b7c8fb487c2eb325c2966849fdf8cd1598fa25
Tomas Knapen <tknapen@gmail.com>
2019-04-01 18:09:07 +0200

    updated code for log and logpolar rfs

--- prfpy/rf.py
+++ prfpy/rf.py
@@ -45,7 +45,7 @@ def gauss1D_log(x, mu=0.0, sigma=1.0):
         gaussian values at log(x)
     """
 
-    return np.exp(-(np.log(x-mu)**2)/(2*sigma**2))
+    return np.exp(-(np.log(x/mu)**2)/(2*sigma**2))
 
 def vonMises1D(x, mu=0.0, kappa=1.0):
     """vonMises1D
@@ -69,10 +69,10 @@ def vonMises1D(x, mu=0.0, kappa=1.0):
     Returns
     -------
     numpy.ndarray
-        von Mises values at x
+        von Mises values at x, peak has y-value of 1
     """
-
-    return stats.vonmises.pdf(x-mu,kappa)
+    vm = stats.vonmises.pdf(x-mu,kappa)
+    return vm / np.max(vm)
 
 def gauss2D_iso_cart(x, y, mu=(0.0,0.0), sigma=1.0):
     """gauss2D_iso_cart
@@ -131,4 +131,41 @@ def gauss2D_rot_cart(x, y, mu=(0.0,0.0), sigma=1.0, theta=0.0, ar=1.0):
     xr = (x-mu[0]) * np.cos(theta) + (y-mu[1]) * np.sin(theta)
     yr = -(x-mu[0]) * np.sin(theta) + (y-mu[1]) * np.cos(theta)
     return np.exp(-(xr**2 + ar**2 * yr**2)/(2*sigma**2))
+
+
+def gauss2D_logpolar(ecc, polar, mu=(0.0,0.0), sigma=1.0, kappa=1.0):
+    """gauss2D_logpolar
+    
+    gauss2D_logpolar takes two-dimensional arrays ecc and polar, containing
+    the eccentricity and polar angle coordinates at which to evaluate the 2D gaussian, 
+    which in this case is a von Mises in the polar angle direction, and a log gauss 
+    in the eccentricity dimension, and returns a 2D array of Z values.
+    We recommend entering the ecc and polar angles ordered as x and y for easy
+    visualization.
+        
+    Parameters
+    ----------
+    ecc : numpy.ndarray, 2D
+        2D, containing eccentricity
+    polar : numpy.ndarray, 2D
+        2D, containing polar angle coordinates (0, 2*np.pi)
+    mu : tuple, optional
+        mean, 2D coordinates of mean/mode of gauss (ecc) and von Mises (polar) (the default is (0.0,0.0))
+    sigma : float, optional
+        standard deviation of gauss (the default is 1.0)
+    kappa : float, optional
+        dispersion coefficient of the von Mises, 
+        akin to invers of standard deviation of gaussian (the default is 1.0)
+        
+    Returns
+    -------
+    numpy.ndarray, 2D
+        values evaluated at (ecc, polar), peak has y-value of 1.
+    """
+    ecc_gauss = np.exp(-(np.log(ecc/mu[0])**2)/(2*sigma**2))
+    polar_von_mises = stats.vonmises.pdf(polar-mu[1],kappa)
+    polar_von_mises /= np.max(polar_von_mises)
+    logpolar_Z = ecc_gauss * polar_von_mises
+    
+    return logpolar_Z / np.max(logpolar_Z)
 
