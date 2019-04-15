5974758620c25cd9e5e3f084d803c012a301989c
Tomas Knapen <tknapen@gmail.com>
2019-04-15 13:17:12 +0200

    updated doc theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,7 +80,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'alabaster'
+pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
@@ -91,7 +91,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'haiku'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
