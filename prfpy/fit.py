defc46e9690387bb9549d3e4d8f559089ca43fc2
marcoaqil <m.aqil@spinozacentre.nl>
2019-08-19 10:15:18 +0200

    dog initial values

--- prfpy/fit.py
+++ prfpy/fit.py
@@ -394,7 +394,7 @@ class DoG_Iso2DGaussianFitter(Iso2DGaussianFitter):
                 self.gridsearch_params, 5, 0.01, axis=-1)
             # surround size
             self.gridsearch_params = np.insert(
-                self.gridsearch_params, 6, 200, axis=-1)
+                self.gridsearch_params, 6, 2*self.gridder.stimulus.max_ecc, axis=-1)
 
         # take exponent and rsq out of the parameters
         parameter_mask = np.arange(self.gridsearch_params.shape[-1] - 2)
