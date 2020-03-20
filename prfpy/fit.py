b59822a2d5b6f84ee2582b86fa056be18cf1bb7e
Marco Aqil <m.aqil@student.vu.nl>
2020-03-20 10:40:46 +0100

    mean hrf

--- prfpy/fit.py
+++ prfpy/fit.py
@@ -303,9 +303,8 @@ class Fitter:
             
         if self.rsq_mask.sum()>0:
             if self.fit_hrf and single_hrf:
-                median_hrf_params = np.average(self.iterative_search_params[self.rsq_mask,-3:-1],
-                                               axis=0,
-                                               weights=self.iterative_search_params[self.rsq_mask,-1])
+                median_hrf_params = np.mean(self.iterative_search_params[self.rsq_mask,-3:-1],
+                                               axis=0)
                 
                 self.iterative_search_params[self.rsq_mask,-3:-1] = median_hrf_params
                 
